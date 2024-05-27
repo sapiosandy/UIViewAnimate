05340ce7e8e4beaf850c32912ad948013508f03b
Sandy Gomez <sgomez1029@gmail.com>
2024-05-27 01:10:07 -0700

    UIViewAnimate

--- UIViewAnimate/ViewController.swift
+++ UIViewAnimate/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var myImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    func animate(whichWay: Bool) {
+        UIView.animate(withDuration: 5.0) {
+        if whichWay {
+            self.myImageView.center.y -= 250
+        } else {
+            self.myImageView.center.y += 250
+        }
+    }
+}
+    
+    @IBAction func startButton(_ sender: UIButton) {
+        animate(whichWay: true)
+    }
+    
+    @IBAction func resetButton(_ sender: UIButton) {
+        animate(whichWay: false)
+    }
 }
 
